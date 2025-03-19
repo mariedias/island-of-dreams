650da04b83106fdf42e36de7c9b4df749667914f
Maria <167498794+mariedias@users.noreply.github.com>
2025-03-19 01:06:42 -0300

    /** @type {import('next').NextConfig} */ const nextConfig = {   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",   images: {     domains: ["islandofdreams.vercel.app"],   }, };
    module.exports = nextConfig;
    
    Fix: Correct export syntax on next.config.mjs and update schema.prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+    images: {
+      domains: ["islandofdreams.vercel.app"],
+    },
+  };
+  
 export default nextConfig;
+  
